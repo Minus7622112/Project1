eebce9867904ec5725c5bc40f4664450c9dbad12
sayhaed <sayvga1359@gmail.com>
2025-03-13 22:49:55 -0600

    Entities and database config

---
+++ Project1/src/main/java/revature/pro0/entity/LoanType.java
@@ -0,0 +1,21 @@
+package revature.pro0.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "loan_types")
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+public class LoanType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "loan_type_id")
+    private Long id;
+
+    @Column(name = "loan_type",nullable = false, unique = true, length = 10)
+    private String type;
+}
